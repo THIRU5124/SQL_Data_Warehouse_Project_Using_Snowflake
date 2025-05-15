2e770d60e0f5c363345e7b340bbd6ff5d8ed05a9
THIRU5124 <thirumaleshpg10@gmail.com>
2025-05-15 17:36:16 +0530

    Add files via upload

---
+++ scripts/init_database_sf.sql
@@ -0,0 +1,28 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DATAWAREHOUSE'. 
+    If the database exists, it is dropped and recreated.
+    It also creates three schemas: 'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this script will drop the entire 'DATAWAREHOUSE' database if it exists. 
+    All data in the database will be permanently deleted. 
+    Make sure you have proper backups before executing.
+=============================================================
+*/
+
+-- Drop and recreate the 'DATAWAREHOUSE' database
+DROP DATABASE IF EXISTS DATAWAREHOUSE;
+
+CREATE DATABASE DATAWAREHOUSE;
+
+-- Switch to the new database
+USE DATABASE DATAWAREHOUSE;
+
+-- Create Schemas
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
